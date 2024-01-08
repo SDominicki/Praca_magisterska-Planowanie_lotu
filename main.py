579bd7623d550f8f00e2147039d5de7ccf1cdbff
Zymi1 <szymon.dominickii@gmail.com>
2024-01-08 12:30:29 +0100

    Clean slate

--- main.py
+++ main.py
@@ -3,27 +3,35 @@ import time
 import math
 from flightgear_python.fg_if import FDMConnection, CtrlsConnection
 
-pid = PID(1, 0.1, 0.1, setpoint=0.0, auto_mode= True, sample_time=0.01)
+pid_roll = PID(5, 1, 0.3, setpoint=0.0, auto_mode= True, sample_time=0.01)
+pid_pitch = PID(5, 1, 0.3, setpoint=0.0, auto_mode= True, sample_time=0.01)
 
 child_aileron_state = 0.0
+child_rudder_state = 0.0
+child_elevator_state = 0.0
 def ctrls_callback(ctrls_data, event_pipe):
     global child_aileron_state
+    global child_rudder_state
+    global child_elevator_state
     if event_pipe.child_poll():
-        child_aileron_req, = event_pipe.child_recv()  # Unpack tuple from parent
+        ctrl_pipe, = event_pipe.child_recv()
+        # Unpack tuple from parent
         # TODO: FG sometimes ignores "once" updates? i.e. if we set `ctrls_data.aileron`
         #  the next callback will still have the old value of `ctrls_data.aileron`, not
         #  the one that we set. To fix this we can just keep our own state of what the value
         #  should be and set it every time. I still need to figure out a clean way to fix
         #  this on the backend
         child_aileron_state = child_aileron_req
+        child_rudder_state = child_rudder_req
+        child_elevator_state = child_elevator_req
     ctrls_data.aileron = child_aileron_state  # from -1..1
     return ctrls_data
 
 def fdm_callback(fdm_data, event_pipe):
     roll_deg = math.degrees(fdm_data.phi_rad) #ROLL
     yaw_deg = math.degrees(fdm_data.psi_rad) #YAW
-    pitch_deg = math.degrees(fdm_data.theta_rad) #PITCH
-    event_pipe.child_send((roll_deg,))  # Send tuple to parent
+    pitch_deg = math.degrees(fdm_data.alpha_rad) #PITCH
+    event_pipe.child_send((pitch_deg,roll_deg)) # Send tuple to parent
 
 """
 Start FlightGear with:
@@ -41,14 +49,26 @@ if __name__ == '__main__':  # NOTE: This is REQUIRED on Windows!
     fdm_conn.start()  # Start the FDM RX loop
 
     roll_deg_setpoint = 0.0  # Let's say the setpoint is 5deg clockwise
+    pitch_deg_setpoint = 0.0
 
     while True:
         if fdm_event_pipe.parent_poll():  # Only update controller when we get FDM updates
-            parent_roll_deg, = fdm_event_pipe.parent_recv()  # Unpack tuple from FDM
+            pipe_data = fdm_event_pipe.parent_recv()  # Unpack tuple from FDM
+            parent_pitch_deg = pipe_data[0]
+            parent_roll_deg = pipe_data[1]
             roll_error = roll_deg_setpoint - parent_roll_deg  # Calculate error
+            pitch_error = pitch_deg_setpoint - parent_pitch_deg
             # Calculate the aileron request (technically should clamp to -1..1 but it doesn't really matter)
-            parent_aileron_req = roll_error * (pid(parent_roll_deg)*0.001)
-            ctrls_event_pipe.parent_send((parent_aileron_req,))  # Send tuple to Ctrls
-            print(pid(parent_roll_deg)*0.001)
+            if (roll_error > 0):
+                parent_aileron_req = roll_error * (pid_roll(parent_roll_deg)*0.001)
+            if (roll_error < 0):
+                parent_aileron_req = roll_error * (pid_roll(parent_roll_deg)*-0.001)
+
+            if (pitch_error > 0):
+                parent_elevator_req = pitch_error * (pid_pitch(parent_pitch_deg)*0.001)
+            if (pitch_error < 0):
+                parent_elevator_req = pitch_error * (pid_pitch(parent_pitch_deg)*-0.001)
+
+            ctrls_event_pipe.parent_send((parent_aileron_req,parent_elevator_req))  # Send tuple to Ctrls
 
         time.sleep(0.01)  # Faster than 30Hz but still sleeping a bit

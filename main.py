edd15224bed81beca3342b5c466dc053272ddcd7
Zymi1 <szymon.dominickii@gmail.com>
2024-01-08 16:23:25 +0100

    Elevator works, rudder needs an overhaul

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import math
 from flightgear_python.fg_if import FDMConnection, CtrlsConnection
 
 roll_deg_setpoint = 0.0  # Let's say the setpoint is 5deg clockwise
-pitch_deg_setpoint = 0.0
-yaw_deg_setpoint = 0.0
+pitch_deg_setpoint = 1.0
+yaw_deg_setpoint = 270
 
 pid_roll = PID(5, 1, 0.3, setpoint=roll_deg_setpoint, auto_mode= True, sample_time=0.01)
-pid_pitch = PID(50, 3, 0.3, setpoint=pitch_deg_setpoint, auto_mode= True, sample_time=0.01)
+pid_pitch = PID(5, 1, 0.3, setpoint=pitch_deg_setpoint, auto_mode= True, sample_time=0.01)
 pid_yaw = PID(5, 1, 0.3, setpoint=yaw_deg_setpoint, auto_mode= True, sample_time=0.01)
 
 child_aileron_state = 0.0
@@ -19,22 +19,22 @@ def ctrls_callback(ctrls_data, event_pipe):
     global child_elevator_state
     global child_rudder_state
     global child_aileron_state
-    if event_pipe.child_poll():
-        child_aileron = event_pipe.child_recv()
-        child_elevator = event_pipe.child_recv()
-        child_rudder = event_pipe.child_recv()
-        # Unpack tuple from parent
-        # TODO: FG sometimes ignores "once" updates? i.e. if we set `ctrls_data.aileron`
-        #  the next callback will still have the old value of `ctrls_data.aileron`, not
-        #  the one that we set. To fix this we can just keep our own state of what the value
-        #  should be and set it every time. I still need to figure out a clean way to fix
-        #  this on the backend
-        child_aileron_state = child_aileron[0]
-        child_elevator_state = child_elevator[0]
-        child_rudder_state = child_rudder[0]
+    if (True):
+        child_aileron,child_elevator,child_rudder  = event_pipe.child_recv()
+    # Unpack tuple from parent
+    # TODO: FG sometimes ignores "once" updates? i.e. if we set `ctrls_data.aileron`
+    #  the next callback will still have the old value of `ctrls_data.aileron`, not
+    #  the one that we set. To fix this we can just keep our own state of what the value
+    #  should be and set it every time. I still need to figure out a clean way to fix
+    #  this on the backend
+    child_aileron_state = child_aileron
+    child_elevator_state = child_elevator
+    child_rudder_state = child_rudder
+    ctrls_data.aileron_trim = 0.0
+    ctrls_data.elevator_trim = 0.0
     ctrls_data.aileron = child_aileron_state  # from -1..1
-    ctrls_data.elevator = child_elevator_state 
-    ctrls_data.rudder = child_rudder_state 
+    ctrls_data.elevator = child_elevator_state
+    ctrls_data.rudder = child_rudder_state
     return ctrls_data
 
 def fdm_callback(fdm_data, event_pipe):
@@ -74,18 +74,17 @@ if __name__ == '__main__':  # NOTE: This is REQUIRED on Windows!
             if (roll_error < 0.0):
                 parent_aileron_req = roll_error * (pid_roll(parent_roll_deg)*-0.001)
 
-            if (pitch_error > 0.0):
-                parent_elevator_req = pitch_error * (pid_pitch(parent_pitch_deg)*0.001)
             if (pitch_error < 0.0):
+                parent_elevator_req = pitch_error * (pid_pitch(parent_pitch_deg)*0.001)
+            if (pitch_error > 0.0):
                 parent_elevator_req = pitch_error * (pid_pitch(parent_pitch_deg)*-0.001)
 
-            if (yaw_error > 0.0):
-                parent_rudder_req = yaw_error * (pid_yaw(parent_yaw_deg)*0.001)
-            if (yaw_error < 0.0):
-                parent_rudder_req = yaw_error * (pid_yaw(parent_yaw_deg)*-0.001)
-            
-            print(parent_elevator_req)
+            if (yaw_error > 5.0):
+                parent_rudder_req = yaw_error * (pid_yaw(parent_yaw_deg)*0.0001)
+            if (yaw_error < -5.0):
+                parent_rudder_req = yaw_error * (pid_yaw(parent_yaw_deg)*-0.0001)
+            if ((yaw_error <= 5.0) & (yaw_error >= -5.0)): 
+                parent_rudder_req = 0.0
 
             ctrls_event_pipe.parent_send((parent_aileron_req,parent_elevator_req,parent_rudder_req))  # Send tuple to Ctrls
-            
-        time.sleep(0.01)  # Faster than 30Hz but still sleeping a bit
+            time.sleep(0.01)  # Faster than 30Hz but still sleeping a bit
